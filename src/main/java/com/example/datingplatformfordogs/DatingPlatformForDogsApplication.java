6088166bd4f26de9aae1e1a69d8a07d8c0d21728
etibarliparvin <etibarliparvin@gmail.com>
2023-03-08 10:08:52 +0400

    first commit

---
+++ src/main/java/com/example/datingplatformfordogs/DatingPlatformForDogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.datingplatformfordogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingPlatformForDogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingPlatformForDogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datingplatformfordogs/DatingPlatformForDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datingplatformfordogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingPlatformForDogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
